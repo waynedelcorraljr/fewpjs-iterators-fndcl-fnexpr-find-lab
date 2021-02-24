b46451ce65be6da5aa554a3e9346c9f11609248e
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-02-23 23:31:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const test = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ] 
 
+
+
+function superbowlWin(record) {
+    let win = record.find(({result}) => result === "W");
+    if (win) {return win.year};
+}
+
+console.log(superbowlWin(test))
